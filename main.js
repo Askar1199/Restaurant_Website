de597ed8286c0e93478d9722032fcb94dae84c93
Mohammed Askar <mdaskar40@gmail.com>
2023-10-13 21:14:26 +0530

    add carousel for bookTable

--- main.js
+++ main.js
@@ -36,5 +36,87 @@ $(document).ready(()=>{
 
 
       // booking gallery--
-     
+      const initializeImageSlider = () => {
+        const images_list = [
+            {
+                "url": "https://html-generator.com/uploads/images/2023/10/13/89474TQVXMBL4Z7.jpg",
+                "alt": "",
+                "name": "89474TQVXMBL4Z7.jpg",
+                "link": ""
+            },
+            {
+                "url": "https://html-generator.com/uploads/images/2023/10/13/460742_JX0_7ZH8.jpg",
+                "alt": "",
+                "name": "460742_JX0_7ZH8.jpg",
+                "link": ""
+            },
+            {
+                "url": "https://html-generator.com/uploads/images/2023/10/13/8235745B51PUSEX.jpg",
+                "alt": "",
+                "name": "8235745B51PUSEX.jpg",
+                "link": ""
+            },
+            {
+                "url": "https://html-generator.com/uploads/images/2023/10/13/020278V964WOVM9.jpg",
+                "alt": "",
+                "name": "020278V964WOVM9.jpg",
+                "link": ""
+            }
+                ];
+            
+                let slider_id = document.querySelector("#hcg-slider-1");
+            
+                // append all images
+                let dots_div = "";
+                let images_div = "";
+                for (let i = 0; i < images_list.length; i++) {
+                    // if no link without href="" tag
+                    let href = (images_list[i].link == "" ? "":' href="'+images_list[i].link+'"');
+                    images_div += '<a'+href+' class="hcg-slides animated"'+(i === 0 ? ' style="display:flex"':'')+'>'+
+                                    
+                                    '<img src="'+images_list[i].url+'" alt="'+images_list[i].name+'">'+
+                                    '<span class="hcg-slide-text"></span>'+
+                                 '</a>';
+                    dots_div += '<a href="#" class="hcg-slide-dot'+(i === 0 ? ' dot-active':'')+'" data-id="'+i+'"></a>';
+                }
+                slider_id.querySelector(".hcg-slider-body").innerHTML = images_div;
+                slider_id.querySelector(".hcg-slide-dot-control").innerHTML = dots_div;
+            
+                let slide_index = 0;
+            
+                const images = slider_id.querySelectorAll(".hcg-slides");
+                const dots = slider_id.querySelectorAll(".hcg-slide-dot");
+                const showSlides = () => {
+                    if (slide_index > images.length-1) {
+                        slide_index = 0;
+                    }
+                    if (slide_index < 0) {
+                        slide_index = images.length-1;
+                    }
+                    for (let i = 0; i < images.length; i++) {
+                        images[i].style.display = "none";
+                        dots[i].classList.remove("dot-active");
+                        if (i == slide_index) {
+                            images[i].style.display = "flex";
+                            dots[i].classList.add("dot-active");
+                        }
+                    }
+                }
+            
+                const dot_click = event => {
+                    event.preventDefault();
+                    slide_index = event.target.dataset.id;
+                    showSlides();
+                }
+            
+                for (let i = 0; i < dots.length; i++) {
+                    dots[i].addEventListener("click", dot_click, false);
+                }
+                // auto play
+                setInterval(() => {
+                    slide_index++;
+                    showSlides();
+                }, 3000);
+      };
+      initializeImageSlider();
 })
